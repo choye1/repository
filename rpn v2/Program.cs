1d409ea08f12d35cf4d02e9a36b8d0b905aa37c3
Choye1 <makcim233237@gmail.com>
2023-11-07 22:12:10 +0500

    parsing, preority

--- rpn v2/Program.cs
+++ rpn v2/Program.cs
@@ -1,62 +1,93 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq.Expressions;
 
+//RPN (Reverse Polish Notation)
 class Program
 {
-    public static void Input()
+    public static void Main() //input, transformation, calculate, output.
     { 
         string userInput = Console.ReadLine();
-        string outputString = Main(userInput);
-        Console.WriteLine("rpn:", outputString);
-        int result = Calculate(outputString);
-        Console.WriteLine("result:", result);
+        List<object> parsedInput = Parsing(userInput);
+        List<object> outputList = Convertation(parsedInput);
+        Console.Write("rpn: ");
+        foreach(object part in outputList) {Console.Write(part.ToString());}
+        /*  int result = Calculate(outputString);
+          Console.WriteLine("result:", result);*/
     }
 
-    public static string Main(string userInput)
+    public static List<object> Parsing(string userInput) //Parsing expression.
     {
-        Stack<string> stack = new Stack<string>();
-        string outputString = "";
-        string digit = "0123456789";
-        for (int i = 0; i < userInput.Length; i++)
+        string num = "";
+        List<object> parsingList = new List<object>();
+        foreach (char variable in userInput)
         {
-            string lastStackVariable = stack.Peek();
-            string variable = Convert.ToString(userInput[i]);
-            switch(variable)
+            if (variable != ' ') 
             {
-                case "1" or "2" or "3" or "3" or "4" or "5" or "6" or "7" or "8" or "9" or "0":
-                    outputString += variable;
-                    break;
-
-                case "+" or "-":
-                    switch (lastStackVariable)
+                if (char.IsDigit(variable)) 
+                {
+                    if (num == "") num += variable;
+                    else
                     {
-                        case "*" or "/":
-                            while (stack.Peek() != "")
-                            {
-                                outputString += stack.Pop();
-                            }
-
-                            break;
-                        default:
-                            stack.Push(variable);
-                            break;
+                        parsingList.Add(num);
+                        num = Convert.ToString(variable);
                     }
+                }
 
-                    break;
-                case "*" or "/" or "(":
-                    stack.Push(variable);
-                    break;
-                case ")":
-                    while (stack.Peek() != "(")
-                    {
-                        outputString += stack.Pop();
-                    }
-                    break;
-                    
-            }
+                else parsingList.Add(variable);
+            } 
+
         }
 
-        return "";
+        if (num != "") parsingList.Add(num);
+        return parsingList;
+    }
+
+    public static int Preority(object operation) 
+    {
+        switch (operation) 
+        {
+            case '+' or '-':
+                return 1;
+            case "/" or "*":
+                return 2;
+            default:
+                return 0;
+        }
+    }
+
+    public static List<object> Convertation(List<object> parsedInput)
+    {
+        Stack<object> stack = new Stack<object>();
+        List<object> output = new List<object>();
+        foreach (object variable in parsedInput)
+        {
+            if (variable is string) output.Add(variable);
+            else if (variable == "(") stack.Push(variable);
+            else if (variable == ")" && stack.Count != 0)
+            {
+                while (stack.Count != 0 && stack.Peek() != "(")
+                {
+                    output.Add(stack.Pop());
+                }
+
+                stack.Pop();
+            }
+
+            else if (stack.Count == 0 || Preority(stack.Peek()) <= Preority(variable)) stack.Push(variable);
+            else
+            {
+                while (stack.Count != 0 || stack.Peek() != "(") output.Add(stack.Pop());
+                stack.Push(variable);
+            }
+            
+        }
+        if (stack.Count > 0)
+        {
+            while (stack.Count>0) output.Add(stack.Pop());
+        }
+
+        return output;
     }
 
     public static int Calculate(string outputString)
@@ -64,3 +95,4 @@ class Program
         return 4;
     }
 }
+
